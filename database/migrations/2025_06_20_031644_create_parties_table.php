6c4bf79dfb60d4b54c3c705e4f9ef5f3cf2cafa2
Cheleizaguirre <Cheleizagab@gmail.com>
2025-07-15 14:45:21 -0600

    Arreglo De Migracion

--- database/migrations/2025_06_20_031644_create_parties_table.php
+++ database/migrations/2025_06_20_031644_create_parties_table.php
@@ -9,22 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('parties', function (Blueprint $table) {
-            $table->string('foto_partido')->nullable()->after('name');
-            $table->string('color_partido', 7)->nullable()->after('foto_partido'); // Para colores hex
-            $table->text('descripcion')->nullable()->after('color_partido');
-        });
-    }
+   public function up(): void
+{
+    Schema::create('parties', function (Blueprint $table) {
+        $table->id();
+        $table->string('name')->unique();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::table('parties', function (Blueprint $table) {
-            $table->dropColumn(['foto_partido', 'color_partido', 'descripcion']);
-        });
+        Schema::dropIfExists('parties');
     }
 };
